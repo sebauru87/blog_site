131879f38cf1c43dfea51348a030e813cf13fced
Sebastian <Ferreira>
2020-06-09 01:55:36 -0300

    add ability to delete post

--- blog/views.py
+++ blog/views.py
@@ -56,6 +56,12 @@ def post_publish(request, pk):
     return redirect('post_detail', pk=pk)
 
 @login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -81,3 +87,4 @@ def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
+
