74b00f4d89ccf56e166c582d0bb94b650d4138e4
Sebastian <Ferreira>
2020-06-10 01:36:14 -0300

    add static file to root

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,4 +125,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blog/static')]
